f36bbb697583e163f9bc84a2d96b439e6cd095ec
敖武 <dongdong.fdd@alibaba-inc.com>
2022-02-04 17:14:09 +0800

    最新发表

--- docs/.vuepress/components/LastPost.vue
+++ docs/.vuepress/components/LastPost.vue
@@ -11,7 +11,7 @@
         <span style="display: flex; align-items: center; margin-right: 5px;">
           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
         </span>
-            <span>{{article.lastUpdated}}</span>
+            <span>{{article.lastUpdated && article.lastUpdated.substring(0, 10)}}</span>
           </div>
         </div>
       </li>
