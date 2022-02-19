00e919d580ca6fd5f16fcc1002c81601c58e2946
敖武 <dongdong.fdd@alibaba-inc.com>
2022-02-19 10:27:16 +0800

    chore: token

--- shell/notice.sh
+++ shell/notice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'https://oapi.dingtalk.com/robot/send?access_token=f93673e87c1b2e8077842fde58b019cea20bd41ec2b7ad3acb7fbceb80984934' -H 'Content-Type: application/json' -d '{"msgtype": "text","text": {"content":"ossblog-'${1}'"}}'
+curl "https://oapi.dingtalk.com/robot/send?access_token=${1}" -H 'Content-Type: application/json' -d '{"msgtype": "text","text": {"content":"ossblog-'${2}'"}}'
