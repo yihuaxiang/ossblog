fdc43fb604d78294bd453d826111ad9968cdda61
敖武 <dongdong.fdd@alibaba-inc.com>
2022-03-01 23:19:58 +0800

    bento

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -149,6 +149,7 @@ module.exports = {
         'ripgrep'
       ],
       '/life/': [
+        'bento',
         'food',
         'firstsnow',
         'snow',
---
+++ docs/life/bento.md
@@ -0,0 +1,5 @@
+# 明日便当
+
+健康又营养的减肥餐
+
+![](https://fudongdong-statics.oss-cn-beijing.aliyuncs.com/images/20220301/6396f306e73847c1af914f762148922f.png)
