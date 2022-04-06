2bf892fed7d4d5e2631868c8145c95b078e39b13
敖武 <dongdong.fdd@alibaba-inc.com>
2022-04-06 23:00:31 +0800

    ssr

--- docs/.vuepress/components/ImgUploader.vue
+++ docs/.vuepress/components/ImgUploader.vue
@@ -27,7 +27,6 @@
 </template>
 
 <script>
-import Notifications from 'vue-notification';
 const axios = require('axios').default;
 export default {
   name: "ImgUploader",
@@ -129,4 +128,7 @@ export default {
 .img-uploader.loading{
 
 }
+</style>
+<style>
+.vue-notification-group{display:block;position:fixed;z-index:5000}.vue-notification-wrapper{display:block;overflow:hidden;width:100%;margin:0;padding:0}.notification-title{font-weight:600}.vue-notification-template{background:#fff}.vue-notification,.vue-notification-template{display:block;box-sizing:border-box;text-align:left}.vue-notification{font-size:12px;padding:10px;margin:0 5px 5px;color:#fff;background:#44a4fc;border-left:5px solid #187fe7}.vue-notification.warn{background:#ffb648;border-left-color:#f48a06}.vue-notification.error{background:#e54d42;border-left-color:#b82e24}.vue-notification.success{background:#68cd86;border-left-color:#42a85f}.vn-fade-enter-active,.vn-fade-leave-active,.vn-fade-move{transition:all .5s}.vn-fade-enter,.vn-fade-leave-to{opacity:0}
 </style>
