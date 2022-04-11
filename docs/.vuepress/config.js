2ce6a2c785f7857e677980d66d24fba81cd80916
敖武 <dongdong.fdd@alibaba-inc.com>
2022-04-11 23:58:10 +0800

    chore: sitemap

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -112,9 +112,6 @@ module.exports = {
     ],
     ['@vuepress/last-updated', {
       transformer: (timestamp, lang) => {
-        //return (new Date(timestamp)).toUTCString() 或者用下面这段
-        // 不要忘了安装 moment
-        const dayjs = require('dayjs')
         dayjs.locale(lang)
         return dayjs(timestamp).toString()
       }
