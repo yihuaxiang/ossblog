94c82ede565f315da56397302336e9b80f757a27
yihuaxiang <fudongdonguestc@gmail.com>
2022-05-09 00:22:50 +0800

    tuchuang domain

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -11,7 +11,7 @@ export default ({
 
   Vue.use(Notifications)
 
-  if(typeof window == 'object' && window.location.host == 'tuchuang.z.wiki') {
+  if (typeof window == 'object' && window.location.host == 'tuchuang.z.wiki') {
     document.body.classList.add('tuchuang-domain');
   }
 
