627825ed448f1968734514f533a5f32e14d2e3b5
yihuaxiang <fudongdonguestc@gmail.com>
2022-08-12 23:09:22 +0800

    feat: ImgPlayer.vue

--- docs/.vuepress/components/ImgPlayer.vue
+++ docs/.vuepress/components/ImgPlayer.vue
@@ -7,15 +7,7 @@
           <img :src="img" loading="lazy" title="敖武的博客-照片" alt="照片加载中..." />
         </template>
         <template v-else>
-          <div class="ctn">
-            <img :src="img.url" loading="lazy" :title="img.desc || '敖武的博客-照片'" :alt="img.title || '敖武的照片'" />
-            <div class="info" :style="{
-              'background-color': img.bgColor || '#9292924f'
-            }">
-              <h3 class="title" v-show="img.title">{{img.title}}</h3>
-              <p class="desc" v-show="img.desc">{{img.desc}}</p>
-            </div>
-          </div>
+          <ImgWithTitle :img="img" />
         </template>
       </swiper-slide>
     </template>
@@ -29,9 +21,11 @@
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
+import ImgWithTitle from "./ImgWithTitle";
 export default {
   name: "ImgPlayer",
   components: {
+    ImgWithTitle,
     Swiper,
     SwiperSlide
   },
@@ -75,35 +69,5 @@ export default {
   align-items: center;
   text-align: center;
   font-weight: bold;
-}
-.ctn {
-  position: relative;
-}
-.ctn .info {
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
-  background: #9292924f;
-  padding: 3px 5px;
-  padding-bottom: 28px;
-  text-align: left;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 10px;
-}
-.ctn .title {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  margin: 0px;
-  font-size: 16px;
-  color: #2c3e50;
-}
-.ctn .desc {
-  margin: 0px;
-  font-size: 14px;
-  color: #2c3e50;
-  font-weight: normal;
 }
 </style>
---
+++ docs/.vuepress/components/ImgWithTitle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="ctn">
+    <img :src="img.url" loading="lazy" :title="img.desc || '敖武的博客-照片'" :alt="img.title || '敖武的照片'" />
+    <div class="info" :style="{
+              'background-color': img.bgColor || '#9292924f'
+            }">
+      <h3 class="title" v-show="img.title">{{img.title}}</h3>
+      <p class="desc" v-show="img.desc">{{img.desc}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImgWithTitle",
+  props: {
+    img: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ctn {
+  position: relative;
+}
+.ctn .info {
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  background: #9292924f;
+  padding: 3px 5px;
+  padding-bottom: 28px;
+  text-align: left;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
+}
+.ctn .title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0px;
+  font-size: 16px;
+  color: #2c3e50;
+}
+.ctn .desc {
+  margin: 0px;
+  font-size: 14px;
+  color: #2c3e50;
+  font-weight: normal;
+}
+</style>
