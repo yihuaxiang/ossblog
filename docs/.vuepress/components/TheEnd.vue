ecbd4f7ed7f2f3e70576028850b1ae9cfe4b1a59
yihuaxiang <fudongdonguestc@gmail.com>
2022-08-14 00:37:43 +0800

    chore:

--- docs/.vuepress/components/TheEnd.vue
+++ docs/.vuepress/components/TheEnd.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="icon" width="200" height="59" src="https://9.z.wiki/autoupload/2022-04-09/b04222e6d4044901ab59a56862ba4bbd.svg" alt="the end" width="200" title="文章到此结束"/>
+  <img class="icon" width="200" height="59" src="https://9.z.wiki/autoupload/2022-04-09/b04222e6d4044901ab59a56862ba4bbd.svg" alt="the end" title="文章到此结束"/>
 </template>
 
 <script>
