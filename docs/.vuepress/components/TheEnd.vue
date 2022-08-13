5ab61e7898f6c76d8b054738537f58e36bb4d063
移花香 <fudongdonguestc@gmail.com>
2022-08-13 10:25:57 +0800

    TheEnd.vue

--- docs/.vuepress/components/TheEnd.vue
+++ docs/.vuepress/components/TheEnd.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="icon" src="https://fudongdong-statics.oss-cn-beijing.aliyuncs.com/autoupload/2022-04-09/b04222e6d4044901ab59a56862ba4bbd.svg" alt="the end" width="200"/>
+  <img class="icon" width="200" height="59" src="https://9.z.wiki/autoupload/2022-04-09/b04222e6d4044901ab59a56862ba4bbd.svg" alt="the end" width="200" title="文章到此结束"/>
 </template>
 
 <script>
