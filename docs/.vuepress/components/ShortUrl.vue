6e032e84c8b122784cbfd8cd63e18ce5bc7be755
敖武 <fudongdong@fudongdong.com>
2022-10-30 00:16:37 +0800

    feat: 短网址

---
+++ docs/.vuepress/components/ShortUrl.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="short-url">
+  短网址
+</div>
+</template>
+
+<script>
+export default {
+  name: "ShortUrl",
+  props: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.short-url{
+  cursor: pointer;
+}
+</style>
---
+++ docs/misc/short-url.md
@@ -0,0 +1,20 @@
+---
+meta:
+  - name: description
+    content: 短网址服务
+  - name: keywords
+    content: 短网址服务
+tags: ["敖武的博客", "短网址服务"]
+sidebar: false
+
+comment: false
+# comments: false
+---
+
+# 短网址服务
+
+### 免费的短网址服务
+
+输入原始网址即可转换为短网址。
+
+<ShortUrl />
