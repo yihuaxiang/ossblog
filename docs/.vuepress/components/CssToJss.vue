9c6abcfd035a4ddd0a7489865d78dacf468e03d0
yihuaxiang <fudongdonguestc@gmail.com>
2022-11-20 15:47:46 +0800

    feat: css to jss

---
+++ docs/.vuepress/components/CssToJss.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="css-to-jss">
+  <div class="wrapper">
+    <table style="width: 100%;">
+      <tbody>
+      <tr>
+        <th style="width: 45%;">CSS</th>
+        <th style="width: 10%; font-size: 22px;">→</th>
+        <th style="width: 45%;">JSS</th>
+      </tr>
+      </tbody>
+    </table>
+    <table>
+      <tbody>
+      <tr>
+        <td style="width: 50%;">
+          <textarea class="code code--input" placeholder="font-size: 18px;
+line-height: 24px;
+color: red;"></textarea>
+        </td>
+        <td style="width: 50%;">
+          <textarea class="code code--output" readonly="">{
+  "fontSize": "18px",
+  "lineHeight": "24px",
+  "color": "red"
+}</textarea>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CssToJss",
+}
+</script>
+
+<style scoped>
+.css-to-jss {
+
+}
+
+table {
+  margin: 0px;
+}
+tbody {
+  display: table;
+  width: 100%;
+}
+
+th {
+  padding: 15px;
+  background: #375eab;
+  text-align: center;
+  color: #fff;
+  font-size: 20px;
+  line-height: 17.5px;
+  border: 0px;
+}
+td {
+  padding: 0px;
+}
+
+.code {
+  -o-box-sizing: border-box;
+  box-sizing: border-box;
+  white-space: pre;
+  width: 100%;
+  display: block;
+  padding: 3%;
+  font: 14px/1.5em "Consolas","Menlo","Monaco","Courier New",monospace;
+  font-weight: 550;
+  outline: none;
+  border: none;
+  resize: none;
+  height: 65vh;
+  overflow: scroll;
+  -moz-tab-size: 4;
+  tab-size: 4;
+}
+.code--input {
+  background: #ecf2f5;
+}
+.code--output {
+  background: #e0ebf5;
+}
+
+</style>
