f9111a81d9b6241ee7f4a29913aa40d86559a837
fudongdong <fudongdonguestc@gmail.com>
2022-12-24 12:25:43 +0800

    gz-icon

--- docs/.vuepress/components/ImgPreview.vue
+++ docs/.vuepress/components/ImgPreview.vue
@@ -80,6 +80,7 @@ export default {
           ['.exe']: 'https://5.z.wiki/autoupload/20221224/PvKj.exe.svg',
           ['.7z']: 'https://0.z.wiki/autoupload/20221224/NpA2.7z.svg',
           ['.rar']: 'https://3.z.wiki/autoupload/20221224/uRGw.rar.svg',
+          ['.gz']: 'https://6.z.wiki/autoupload/20221224/GUg7.gzip.svg',
           ['.dmg']: 'https://5.z.wiki/autoupload/20221224/0IXD.dmg.svg'
         };
         let icon = 'https://5.z.wiki/autoupload/2022-09-18/a426d828fa6242cdb9698fbe32a430a4.unknownfile.svg';
