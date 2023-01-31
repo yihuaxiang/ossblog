e2612995a084d7241afcd710bc0f7128f2d7cbe6
fudongdong <fudongdonguestc@gmail.com>
2023-01-31 20:08:01 +0800

    宝塔

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -201,6 +201,7 @@ module.exports = {
     ],
     sidebar: {
       '/tech/': [
+        'baota',
         'aspx',
         'db',
         'centos-set-swap',
---
+++ docs/tech/baota.md
@@ -0,0 +1,6 @@
+# 宝塔 linux 面板
+
+宝塔`linux`面板是个好东西，在`docker`里面体验了下，方便很多。
+
+
+![](https://z.wiki/autoupload/20230131/LH4e.168X286-image.png)
