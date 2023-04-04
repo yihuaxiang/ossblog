2dd9f4ed37c822991a7e34e7d7c04711ae995dff
fudongdong <fudongdonguestc@gmail.com>
2023-04-04 22:35:00 +0800

    apps

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -190,6 +190,7 @@ module.exports = {
         ariaLabel: '下载',
         items: [
           {text: '系统镜像', link: 'https://z.wiki/resource/system-mirror-iso.html'},
+          {text: '破解软件', link: 'https://z.wiki/resource/apps.html'},
         ]
       },
       {text: '生活', link: '/life/'},
