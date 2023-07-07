d218023b989d202020832e3a2bc904032df69803
fudongdong <fudongdonguestc@gmail.com>
2023-07-07 20:32:20 +0800

    web-frame

--- docs/.vuepress/components/WebFrame.vue
+++ docs/.vuepress/components/WebFrame.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe width="100%" class="web-frame" :src="src" />
+  <iframe class="web-frame" :src="src" />
 </template>
 
 <script>
@@ -16,8 +16,15 @@ export default {
 
 <style scoped>
 .web-frame {
-  margin-top: 15px;
-  margin-bottom: 10px;
-  background-image: url("https://z.wiki/autoupload/20230707/9Tzi.1752X2456-image.png");
+    margin-top: 15px;
+    margin-bottom: 10px;
+    background-image: url(https://z.wiki/autoupload/20230707/9Tzi.1752X2456-image.png);
+    border: none;
+    background-size: 100%;
+    padding-top: 50px;
+    padding-left: 35px;
+    width: -webkit-fill-available;
+    padding-right: 40px;
+    height: 470px;
 }
 </style>
