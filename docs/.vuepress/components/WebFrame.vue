a1d7ac9d1e3c978e357b83e9af8c2b879c983923
fudongdong <fudongdonguestc@gmail.com>
2023-07-07 20:20:41 +0800

    web-frame

---
+++ docs/.vuepress/components/WebFrame.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe width="100%" class="web-frame" :src="src" />
+</template>
+
+<script>
+export default {
+  name: "WebFrame",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.web-frame {
+  margin-top: 15px;
+  margin-bottom: 10px;
+}
+</style>
