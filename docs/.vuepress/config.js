534d76128457c58d90960078ec60ff6707de90b6
fudongdong <fudongdonguestc@gmail.com>
2023-08-26 00:20:15 +0800

    link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -180,7 +180,7 @@ module.exports = {
           {text: '表情制作', link: 'https://sorry.xuty.tk/wangjingze/'},
           {text: '二维码', link: 'https://z.wiki/misc/qrcode.html'},
           {text: '徽章', link: 'https://shields.io/'},
-          {text: 'jsbin', link: 'https://z.wiki/jsbin/'},
+          {text: '实验室', link: 'https://z.wiki/jsbin/'},
           {text: '短网址', link: 'https://z.wiki/misc/short-url.html'},
           {text: 'adcode 查询', link: 'https://playground.z.wiki/lbs/showCityInfo'},
         ]
