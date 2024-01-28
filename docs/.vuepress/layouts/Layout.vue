87c5919c2f8d5d2fd79a31cb51c0bc62a5f83dde
fudongdong <fudongdonguestc@gmail.com>
2024-01-28 14:57:43 +0800

    layout

---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #navbar-before>
+      <div class="my-footer">This is my custom page footer</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.my-footer {
+  text-align: center;
+}
+</style>
