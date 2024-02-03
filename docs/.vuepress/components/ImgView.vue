70efa77066cb753f6a43c9d4c8024b04b6ffc6e2
fudongdong <fudongdonguestc@gmail.com>
2024-02-03 21:54:12 +0800

    loading

--- docs/.vuepress/components/ImgView.vue
+++ docs/.vuepress/components/ImgView.vue
@@ -67,7 +67,7 @@ export default {
 
   animation: rectangle infinite 1s ease-in-out -0.2s;
 
-  background-color: #eee;
+  background-color: #666;
 }
 
 .loading:before,
@@ -76,7 +76,7 @@ export default {
   width: 6px;
   height: 10px;
   content: "";
-  background-color: #eee;
+  background-color: #666;
 }
 
 .loading:before {
@@ -96,12 +96,12 @@ export default {
   80%,
   100% {
     height: 20px;
-    box-shadow: 0 0 #eee;
+    box-shadow: 0 0 #666;
   }
 
   40% {
     height: 30px;
-    box-shadow: 0 -20px #eee;
+    box-shadow: 0 -20px #666;
   }
 }
 .loading-ctn {
