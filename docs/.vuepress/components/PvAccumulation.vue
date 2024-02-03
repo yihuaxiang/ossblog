b41f71589026a2ef3ceb22b347777f49a8318d45
fudongdong <fudongdonguestc@gmail.com>
2024-02-03 12:05:57 +0800

    chore

--- docs/.vuepress/components/PvAccumulation.vue
+++ docs/.vuepress/components/PvAccumulation.vue
@@ -28,7 +28,7 @@ export default {
         }
         if (typeof fetch != undefined) {
           try {
-            fetch(`https://playground.z.wiki/blog/accumulation?url=${encodeURIComponent(path)}`)
+            fetch(`https://playground.z.wiki/blog/accumulation?url=${encodeURIComponent(path)}&host=${encodeURIComponent(window.location.host)}`)
               .then(res => res.text()).then(pv => {
               this.pv = pv;
             })
