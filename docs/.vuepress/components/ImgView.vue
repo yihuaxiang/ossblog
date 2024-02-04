758da9bafce2331ef8fab9f9004f4f266fff1d2c
fudongdong <fudongdonguestc@gmail.com>
2024-02-04 22:46:05 +0800

    loading

--- docs/.vuepress/components/ImgView.vue
+++ docs/.vuepress/components/ImgView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div v-show="isLoading" class="loading-container">
-      <div class="loader"></div>
+    <div v-show="isLoading" class="loading-container-div">
+      <div class="lds-hourglass"></div>
     </div>
     <img v-show="!isLoading" class="img-view" ref="img" :src="url" :alt="title" :title="title" :style="imgStyle" />
   </div>
@@ -138,6 +138,45 @@ export default {
     transform: rotate(360deg);
   }
 }
+
+
+
+
+
+
+
+.lds-hourglass {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-hourglass:after {
+  content: " ";
+  display: block;
+  border-radius: 50%;
+  width: 0;
+  height: 0;
+  margin: 8px;
+  box-sizing: border-box;
+  border: 32px solid #000;
+  border-color: #000 transparent #000 transparent;
+  animation: lds-hourglass 1.2s infinite;
+}
+@keyframes lds-hourglass {
+  0% {
+    transform: rotate(0);
+    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+  50% {
+    transform: rotate(900deg);
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+  100% {
+    transform: rotate(1800deg);
+  }
+}
+
 
 </style>
 
