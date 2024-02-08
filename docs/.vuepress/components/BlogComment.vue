3b580de0fc25dc5b6604cb36e492ab037cfb4ee9
fudongdong <fudongdonguestc@gmail.com>
2024-02-08 17:09:48 +0800

    chore

---
+++ docs/.vuepress/components/BlogComment.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="blog-comment">
+</div>
+</template>
+
+<script>
+import fetch from 'cross-fetch';
+export default {
+  name: "BlogComment",
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  watch: {
+    '$route.path': {
+      immediate: true,
+      handler: function() {
+        const path = this.$route.path;
+        console.log('path is', path);
+
+        if (typeof fetch != undefined) {
+          try {
+            fetch(`https://playground.z.wiki/comment/list?url=${encodeURIComponent(path)}`)
+              .then(res => res.text()).then(info => {
+                console.log('info is', info);
+            })
+          } catch(e) {
+            console.warn('failed to fetch accumulation', e);
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.blog-comment{
+  display: none;
+}
+</style>
+
