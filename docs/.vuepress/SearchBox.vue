23db2c03fe45a56bb40029f00970de2f7a7a4a1c
fudongdong <fudongdonguestc@gmail.com>
2024-02-15 16:40:59 +0800

    搜索

--- docs/.vuepress/SearchBox.vue
+++ docs/.vuepress/SearchBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search-box">
-    <input type="text" v-model="keyword" />
+    <input type="text" v-model="keyword" placeholder="搜索" @input="handleInput" />
   </div>
 </template>
 <script>
@@ -9,6 +9,11 @@ export default {
   data() {
     return {
       keyword: ''
+    }
+  },
+  methods: {
+    handleInput() {
+      console.log('handleInput')
     }
   },
   watch: {
