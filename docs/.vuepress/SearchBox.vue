9974725b4a6b73187efdf971a909ad8e04425d6c
fudongdong <fudongdonguestc@gmail.com>
2024-02-15 16:37:43 +0800

    搜索

--- docs/.vuepress/SearchBox.vue
+++ docs/.vuepress/SearchBox.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="search-box">
-    search box
+    <input type="text" v-model="keyword" />
   </div>
 </template>
 <script>
 export default {
-  name: 'SearchBox'
+  name: 'SearchBox',
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+  watch: {
+    keyword() {
+      console.log('keyword is', keyword);
+    }
+  }
 }
 </script>
