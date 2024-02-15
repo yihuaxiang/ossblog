165dc3b9458d432c28a99e51142f9f049a93a41d
fudongdong <fudongdonguestc@gmail.com>
2024-02-15 15:36:28 +0800

    分页

--- docs/.vuepress/components/BlogComment.vue
+++ docs/.vuepress/components/BlogComment.vue
@@ -64,7 +64,13 @@
       </template>
       <div class="pages">
         <template v-for="pageIndex in totalPage">
-          <span class="page" @click="page = pageIndex - 1">{{pageIndex}}</span>
+          <span 
+            class="page" 
+            @click="page = pageIndex - 1"
+            :class="{active: page == pageIndex - 1}"
+          >
+            {{pageIndex}}
+          </span>
         </template>
       </div>
     </div>
@@ -377,6 +383,10 @@ export default {
        margin-left: 5px;
        margin-right: 5px;
        font-size: 14px;
+
+       &.active {
+         background-color: rgb(211 211 211);
+       }
     }
   }
 }
