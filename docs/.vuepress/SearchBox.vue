89eeedd14acb5e92b4aa1384c2cad0d901779729
敖武 <dongdong.fdd@alibaba-inc.com>
2024-02-15 17:15:53 +0800

    feat: 搜索框

--- docs/.vuepress/SearchBox.vue
+++ docs/.vuepress/SearchBox.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="sb-ctn">
     <div class="search-box">
-      <input type="text" v-model="keyword" placeholder="搜索" @input="handleInput" />
+      <input type="text" v-model="keyword" placeholder="搜索" @input="handleInput" @blur="handleBlur" />
     </div>
-    <div class="list">
-
+    <div class="list" v-if="list && list.length > 0">
+      <div v-for="item in list">
+        <a :href="item.link">
+          <p v-html="item.title">{{item.title}}</p>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -18,12 +22,24 @@ export default {
     }
   },
   methods: {
+    handleBlur() {
+      this.keyword = '';
+      this.list = [];
+    },
     handleInput(e) {
       console.log('handleInput', e.target.value);
       const value = e.target.value;
       fetch(`https://playground.z.wiki/search/index?keyword=${value}`).then(res => res.json()).then(data => {
         console.log('data is', data);
-        this.list = data || [];
+        this.list = (data || []).map(item => {
+          item.title = item.title
+            .replace(/__START_TAG__/g, '<b>')
+            .replace(/__END_TAG__/g, '</b>');
+          item.content = item.content
+            .replace(/__START_TAG__/g, '<b>')
+            .replace(/__END_TAG__/g, '</b>');
+          return item;
+        });
       })
     }
   },
@@ -34,3 +50,59 @@ export default {
   }
 }
 </script>
+<style scoped>
+.sb-ctn {
+
+  .search-box {
+    display: inline-block;
+    position: relative;
+    margin-right: 1rem;
+
+    input {
+      cursor: text;
+      width: 10rem;
+      height: 2rem;
+      color: #4e6e8e;
+      display: inline-block;
+      border: 1px solid #cfd4db;
+      border-radius: 2rem;
+      font-size: .9rem;
+      line-height: 2rem;
+      padding: 0 .5rem 0 2rem;
+      outline: none;
+      transition: all .2s ease;
+      background: #fff url(/assets/img/search.83621669.svg) .6rem .5rem no-repeat;
+      background-size: 1rem;
+    }
+  }
+
+  .list {
+    max-height: 200px;
+    overflow: auto;
+    max-width: 200px;
+    padding: 5px;
+    padding-left: 10px;
+    border-radius: 6px;
+    border: 1px #ccc solid;
+    margin-top: 5px;
+    line-height: 0px;
+
+    a {
+      color: unset;
+      font-weight: normal;
+      display: inline-block;
+      padding-top: 2px;
+      padding-bottom: 2px;
+    }
+
+    p {
+      margin: 0px;
+      padding: 0px;
+    }
+
+    b {
+      color: red;
+    }
+  }
+}
+</style>
