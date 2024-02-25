706ee89102c14c9cb24cd0e8839686dbd814d623
fudongdong <fudongdonguestc@gmail.com>
2024-02-25 15:39:02 +0800

    adcode-query

--- update_index.sh
+++ update_index.sh
@@ -13,13 +13,13 @@ find . -name '*.md' -print0 | while IFS= read -r -d $'\0' file; do
     # 读取文件内容，使用一个工具如 cat 或者 sed 来获取文件完整内容
     # 根据需要清洗文件内容，使其适合作为 HSET 的值
     # 注意: 如果内容包含特殊字符（如引号），你需要适当地转义它们
-    content=$(cat "$file" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' | sed -E 's/^#+//')
+    content=$(cat "$file" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' | sed -E 's/^#+//' | sed 's/[[:space:]]\{1,\}/ /g')
 
     # 生成文件路径（移除'./'前缀并替换空格为'-'）
     file_path=$(echo "$file" | sed 's/^.\///' | sed 's/ /-/g' | sed 's/\.md//' | sed 's/README/index/')
     curl "https://playground.z.wiki/comment/list?path=/${file_path}.html" -o tmp.file
     comments=$(cat tmp.file | jq . | grep comment | awk -F '"comment"' '{print $2}')
-    comments=$(echo "$comments" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' | sed 's/\\\\"/\\"/g')
+    comments=$(echo "$comments" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' | sed 's/\\\\"/\\"/g' | sed 's/[[:space:]]\{1,\}/ /g')
     content="${content}${comments}"
     
 
