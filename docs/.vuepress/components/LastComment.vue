5ce1ab79c69f61fce04012b8a36b570bbafeeb61
fudongdong <fudongdonguestc@gmail.com>
2024-03-21 23:10:00 +0800

    comment

--- docs/.vuepress/components/LastComment.vue
+++ docs/.vuepress/components/LastComment.vue
@@ -5,9 +5,9 @@
   <ul>
     <template v-for="(comment, index) in topComments">
       <li :key="comment.id">
-        <div style="margin-right: 20px; display: flex; justify-content: space-between;">
+        <div style="margin-right: 20px; display: flex; justify-content: space-between;white-space: nowrap;">
           <a class="comment-link" :href="comment.url" @click.prevent="pushRoute(comment.url)" v-html="comment.comment"></a>
-          <div style="display: flex; align-items: center; font-size: 12px; color: rgba(0, 0, 0, 0.54); font-weight: 200;">
+          <div style="display: flex; align-items: center; font-size: 12px; color: rgba(0, 0, 0, 0.54); font-weight: 200; flex-grow: 1;">
             <span>{{comment.nick}}</span>
           </div>
         </div>
@@ -78,8 +78,15 @@ export default {
 .comment-link {
   display: flex;
   align-items: center;
+  text-overflow: ellipsis;
+  padding-right: 5px;
+  flex-shrink: 9;
+  white-space: nowrap;
+  display: inline-block;
+  overflow: hidden;
 }
 .comment-link /deep/ img {
   height: 18px;;
 }
+
 </style>
